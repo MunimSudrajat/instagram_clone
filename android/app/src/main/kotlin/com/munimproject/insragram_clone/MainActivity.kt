d15adb4a4bfd9a3af0c016a54150cb0bc7895bbc
MunimSudrajat <abdulmunim905@gmail.com>
2024-02-28 11:32:02 +0700

    p

---
+++ android/app/src/main/kotlin/com/munimproject/insragram_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.munimproject.insragram_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/munimproject/instagram_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.munimproject.instagram_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
